2f8a3b49206f760820e57013ecd9a8b4fc201e91
ViewtifulJim <jamescolinwilliams@outlook.com>
2025-06-11 14:28:35 +0100

    Line Edits

---
+++ LineEdits.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(1050, 500, 500, 500)
+        self.line_edit = QLineEdit(self)
+        self.button = QPushButton("Submit", self)
+        self.setWindowTitle("Line Edits")
+
+        self.initUI()
+
+    def initUI(self):
+        self.line_edit.setGeometry(10, 10, 200, 40)
+        self.button.setGeometry(210, 10, 100, 40)
+        self.line_edit.setStyleSheet("font-size: 25px;"
+                                     "font-family: sans-serif;")
+        self.button.setStyleSheet("font-size: 25px;"
+                                     "font-family: sans-serif;")
+
+        self.line_edit.setPlaceholderText("Enter your name")
+
+        self.button.clicked.connect(self.submit)
+
+    def submit(self):
+        text = self.line_edit.text()
+        print(f"Hello, {text}")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
